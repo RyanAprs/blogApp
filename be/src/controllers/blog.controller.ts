12bda3ea49613e182b5343c425175443794b07ec
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-04-20 10:32:29 +0700

    add full routing and controller

--- be/src/controllers/blog.controller.ts
+++ be/src/controllers/blog.controller.ts
@@ -3,3 +3,15 @@ import { Request, Response } from "express";
 export const getBlog = (req: Request, res: Response) => {
   res.send("Get all blogs");
 };
+
+export const createBlog = (req: Request, res: Response) => {
+  res.send("Create blog");
+};
+
+export const updateBlog = (req: Request, res: Response) => {
+  res.send("Update blog");
+};
+
+export const deleteBlog = (req: Request, res: Response) => {
+  res.send("Delete blog");
+};
--- be/src/routes/blog.route.ts
+++ be/src/routes/blog.route.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { getBlog } from "../controllers/blog.controller";
+import {
+  createBlog,
+  deleteBlog,
+  getBlog,
+  updateBlog,
+} from "../controllers/blog.controller";
 
 export const Blogrouter: Router = Router();
 
 Blogrouter.get("/", getBlog);
-
+Blogrouter.get("/:id", getBlog);
+Blogrouter.post("/", createBlog);
+Blogrouter.put("/:id", updateBlog);
+Blogrouter.delete("/:id", deleteBlog);
