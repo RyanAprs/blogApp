ad8dd67442e5ae8610ffb7524d0cebc82bb39c4f
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-04-26 11:13:07 +0700

    login & register routes

--- fe/src/config/routes/index.tsx
+++ fe/src/config/routes/index.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home/home";
+import Login from "../../pages/Login/login";
+import Register from "../../pages/Register/register";
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
