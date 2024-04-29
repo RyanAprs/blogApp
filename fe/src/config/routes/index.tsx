2765c189a398053264e9b468a45334bd4fa8bb04
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-04-29 11:24:07 +0700

    update routing

--- fe/src/config/routes/index.tsx
+++ fe/src/config/routes/index.tsx
@@ -9,66 +9,52 @@ import DetailBlog from "../../pages/Blogs/detailBlog";
 import Profile from "../../pages/Profile/profile";
 import UpdateProfile from "../../pages/Profile/updateProfile";
 
+const RouteData = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/blog",
+    element: <Blogs />,
+  },
+  {
+    path: "/blog/:id",
+    element: <DetailBlog />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/profile/:id",
+    element: <Profile />,
+  },
+  {
+    path: "/update/:id",
+    element: <UpdateProfile />,
+  },
+];
+
 const Routing = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <Home />
-            </>
-          }
-        />
-        <Route
-          path="/blog"
-          element={
-            <>
-              <Header />
-              <Blogs />
-            </>
-          }
-        />
-        <Route
-          path="/blog/:id"
-          element={
-            <>
-              <Header />
-              <DetailBlog />
-            </>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <>
-              <Header />
-              <Contact />
-            </>
-          }
-        />
-        <Route
-          path="/profile/:id"
-          element={
-            <>
-              <Header />
-              <Profile />
-            </>
-          }
-        />
-        <Route
-          path="/update/:id"
-          element={
-            <>
-              <Header />
-              <UpdateProfile />
-            </>
-          }
-        />
+        {RouteData.map((route) => {
+          return (
+            <Route
+              path={route.path}
+              element={
+                <>
+                  <Header />
+                  {route.element}
+                </>
+              }
+            />
+          );
+        })}
       </Routes>
     </Router>
   );
---
+++ fe/src/pages/Profile/blogUser.tsx
@@ -0,0 +1,5 @@
+const BlogUser = () => {
+  return <div>Blog User Page</div>;
+};
+
+export default BlogUser;
