e45adb874fc6f53dfa94c92ae8abc53feb48894e
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-07 23:38:46 +0700

    responsive navbar

--- fe/src/components/molecules/Header/header.tsx
+++ fe/src/components/molecules/Header/header.tsx
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import DropdownMenu from "../dropDown/dropDown";
+import { FaUser } from "react-icons/fa";
 
 const Header: React.FC = () => {
   const [user, setUser] = useState<any | null>(null);
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const getUserDataFromCookie = () => {
@@ -54,7 +74,7 @@ const Header: React.FC = () => {
           </h1>
         </Link>
       </div>
-      <nav className="text-lg text-white flex gap-8 font-bold">
+      <nav className="text-lg text-white hidden gap-8 font-bold md:flex">
         <Link to="/" className="hover:text-gray-500 transition-all">
           <h1>Home</h1>
         </Link>
@@ -66,7 +86,68 @@ const Header: React.FC = () => {
         </Link>
       </nav>
       <div>
-        <DropdownMenu handleLogout={handleLogout} user={user} />
+        <div className="relative" ref={dropdownRef}>
+          <div className="flex items-center">
+            {user && user.name && <p className="mr-2">{user.name}</p>}
+            {user ? (
+              <button onClick={toggleDropdown}>
+                <img
+                  src={`http://localhost:3000/${user.image}`}
+                  alt="user image"
+                  className="h-[40px] w-[40px] object-cover rounded-full bg-gray-200"
+                />
+              </button>
+            ) : (
+              <button
+                onClick={toggleDropdown}
+                className="cursor-pointer p-3 bg-gray-200 rounded-full"
+              >
+                <FaUser className="text-black " />
+              </button>
+            )}
+          </div>
+          {isOpen && (
+            <div className="absolute top-full left-0 mt-2 bg-white border border-gray-300 rounded shadow-lg">
+              {user ? (
+                <div className="flex flex-col">
+                  <Link
+                    to={`/profile/${user.user_id}`}
+                    className="px-6 py-2 block w-full text-left text-gray-800 hover:bg-gray-200"
+                  >
+                    Profile
+                  </Link>
+                  <nav className="text-left block md:hidden">
+                    <Link
+                      to="/blog"
+                      className="px-6 py-2 block w-full text-gray-800 hover:bg-gray-200"
+                    >
+                      <h1>Blogs</h1>
+                    </Link>
+                    <Link
+                      to="/contact"
+                      className="px-6 py-2 block w-full text-gray-800 hover:bg-gray-200"
+                    >
+                      <h1>Contact</h1>
+                    </Link>
+                  </nav>
+                  <button
+                    onClick={handleLogout}
+                    className="px-6 py-2 block w-full text-left text-gray-800 hover:bg-gray-200"
+                  >
+                    Logout
+                  </button>
+                </div>
+              ) : (
+                <Link
+                  to="/login"
+                  className="block w-full text-left py-2 px-4 text-gray-800 hover:bg-gray-200"
+                >
+                  Login
+                </Link>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
